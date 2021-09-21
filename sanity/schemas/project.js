473a572421e72f64e07dc4d7368baf8d4b7923c5
DerLev <levin.schroeren@gmail.com>
2021-09-21 19:34:54 +0200

    init sanity.io

---
+++ sanity/schemas/project.js
@@ -0,0 +1,66 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'status',
+      title: 'Status',
+      type: 'string',
+      options: {
+        list: [
+          { value: 'planned', title: 'Planned' },
+          { value: 'dev', title: 'Development' },
+          { value: 'done', title: 'Done & Maintenance' },
+          { value: 'archive', title: 'Archived' },
+        ]
+      },
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'},
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    },
+    {
+      name: 'url',
+      title: 'GitHub URL',
+      type: 'url',
+    }
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      })
+    },
+  },
+}
