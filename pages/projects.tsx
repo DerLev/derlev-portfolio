db4d009891143d09ac5e3bf54043e2dc2707e049
DerLev <levin.schroeren@gmail.com>
2021-09-24 20:11:04 +0200

    projects page finished

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -50,7 +50,7 @@ const sortStatus = (data: any, asc?: boolean) => {
 const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const [data, setData] = useState<any>(projects);
   const [sorting, setSorting] = useState<any>(sortings[0]);
-  const [modalOpen, setModalOpen] = useState<boolean>(true);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [modalObject, setModalObject] = useState(projects[0]);
 
   const sort = (d:any) => {
@@ -77,6 +77,11 @@ const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStat
     setSorting(d);
   }
 
+  const openModal = (i:number) => {
+    setModalObject(data[i]);
+    setModalOpen(true);
+  }
+
   return (
     <>
       <Transition show={modalOpen} as={Fragment}>
@@ -91,7 +96,7 @@ const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStat
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+              <Dialog.Overlay className="fixed inset-0" style={{ background: '#0000004c' }} />
             </Transition.Child>
             <Transition.Child
               as={Fragment}
@@ -102,12 +107,91 @@ const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStat
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="relative bg-white rounded max-w-sm mx-auto text-gray-800">
-                <h1>{modalObject.title}</h1>
-                {
-                  modalObject.description &&
-                  <BlockContent blocks={modalObject.description[0]} />
-                }
+              <div className="relative bg-white rounded-lg max-w-md mx-auto text-gray-800 grid w-full transform">
+                <div className="bg-img">
+                  <Image src={urlFor(modalObject.mainImage).width(800).height(460).url()} width={400} height={230} alt="Project Image" layout="responsive" className="rounded-lg" />
+                </div>
+                <div className="fg-text z-10 text-white rounded-lg p-2 flex flex-col justify-between" style={{ background: "linear-gradient(180deg, rgba(0,0,0,0) 50%, rgba(0,0,0,0.5) 100%)" }}>
+                  <div>
+                    <h1 className="text-2xl title-shadow">{modalObject.title}</h1>
+                    {
+                      modalObject.description &&
+                      <div className="description-shadow">
+                        <BlockContent blocks={modalObject.description[0]} />
+                      </div>
+                    }
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex flex-col justify-end flex-shrink-0">
+                      {
+                        (() => {
+                          switch (modalObject.status) {
+                            case 'planned':
+                              return (
+                                <div className="flex items-center gap-1 text-gray-300 italic text-shadow">
+                                  <ClockIcon className="w-4 h-4" />
+                                  <span>Planned</span>
+                                </div>
+                              )
+                            case 'dev':
+                              return (
+                                <div className="flex items-center gap-1 text-white text-shadow">
+                                  <CodeIcon className="w-4 h-4" />
+                                  <span>Development</span>
+                                </div>
+                              )
+                            case 'done':
+                              return (
+                                <div className="flex items-center gap-1 text-green-300 text-shadow">
+                                  <CheckIcon className="w-4 h-4" />
+                                  <span>Done (Maintenance)</span>
+                                </div>
+                              )
+                            case 'archive':
+                              return (
+                                <div className="flex items-center gap-1 text-gray-300 text-shadow">
+                                  <ArchiveIcon className="w-4 h-4" />
+                                  <span>Archived</span>
+                                </div>
+                              )
+                          }
+                        })()
+                      }
+                      <div className="flex items-center gap-1 text-shadow">
+                        <Image src={urlFor(modalObject.author.image).width(32).height(32).url()} width={16} height={16} className="rounded-full" alt="Author Image" />
+                        <span>{modalObject.author.name}</span>
+                      </div>
+                      {
+                        modalObject.url && !modalObject.private &&
+                        <div className="text-gray-300 text-shadow">
+                          <a href={modalObject.url} rel="noopener noreferrer" target="_blank" className="flex items-center gap-1">
+                            <CodeIcon className="w-4 h-4" />
+                            <span>Open on GitHub</span>
+                          </a>
+                        </div>
+                      }
+                      {
+                        modalObject.private &&
+                        <div className="text-yellow-300 text-shadow flex items-center gap-1">
+                          <LockClosedIcon className="w-4 h-4" />
+                          <span>Private project</span>
+                        </div>
+                      }
+                    </div>
+                    {
+                      modalObject.tags &&
+                      <div className="text-right pl-4">
+                        {
+                          modalObject.tags.map((t:string, i:number) => (
+                            <div className="bg-blue-500 px-3 rounded-full inline-block m-px shadow-lg" key={i}>
+                              <span>{t}</span>
+                            </div>
+                          ))
+                        }
+                      </div>
+                    }
+                  </div>
+                </div>
               </div>
             </Transition.Child>
           </div>
@@ -143,12 +227,12 @@ const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStat
       </div>
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4 px-4">
         {
-          data && data.map((project:any) => (
+          data && data.map((project:any, i:number) => (
             <div key={project._id} className="w-full relative grid">
               <div className="bg-img">
                 <Image src={urlFor(project.mainImage).width(800).height(460).url()} width={400} height={230} className="rounded-lg" alt="Project Image" layout="responsive" />
               </div>
-              <div className="fg-text z-10 rounded-lg bg-gray-600 grid p-1 group" style={{ background: "linear-gradient(180deg, rgba(0,0,0,0) 50%, rgba(0,0,0,0.5) 100%)", gridTemplateRows: '2fr 1fr' }}>
+              <div className="fg-text z-10 rounded-lg bg-gray-600 grid p-1 cursor-pointer" style={{ background: "linear-gradient(180deg, rgba(0,0,0,0) 50%, rgba(0,0,0,0.5) 100%)", gridTemplateRows: '2fr 1fr' }} onClick={() => openModal(i)}>
                 <div className="flex justify-center items-center flex-col gap-1">
                   <h1 className="text-3xl title-shadow text-center">{project.title}</h1>
                   {
@@ -200,10 +284,10 @@ const Projects: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStat
                   {
                     project.url && !project.private &&
                     <div className="text-gray-300 text-shadow">
-                      <a href={project.url} rel="noopener noreferrer" target="_blank" className="flex items-center gap-1">
+                      <div className="flex items-center gap-1">
                         <CodeIcon className="w-4 h-4" />
                         <span>Open on GitHub</span>
-                      </a>
+                      </div>
                     </div>
                   }
                   {
