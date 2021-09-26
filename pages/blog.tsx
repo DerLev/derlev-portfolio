3175fb9c6cef09a16db0e6d4130319687c65d3d3
DerLev <levin.schroeren@gmail.com>
2021-09-26 14:24:03 +0200

    blog.tsx done

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,10 +1,83 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next'
+import client, { urlFor } from '../components/sanityClient'
+import { NextSeo } from 'next-seo'
+import Image from 'next/image'
+import moment from 'moment'
+import Link from 'next/link'
 
-const Blog: NextPage = () => {
-
+const Blog: NextPage = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <h1>blog</h1>
+    <>
+      <NextSeo
+        title="Blog"
+        description="This is my blog. Here I write about my progress of projects and other stuff."
+        openGraph={{
+          url: process.env.rootUrl + 'blog'
+        }}
+      />
+      <h1 className="text-center text-3xl mb-4 font-semibold">Blog</h1>
+      <h2 className="text-center text-xl mb-4 font-medium">This is my blog. Here I write about my progress of projects and other stuff.</h2>
+      <div className="grid grid-cols-1 gap-2">
+        {
+          posts.map((post:any) => (
+            <div className="grid shadow-md" key={post._id}>
+              <div className="bg-img overflow-hidden relative h-28 md:h-20 rounded-lg">
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 filter blur-sm">
+                  <Image src={urlFor(post.mainImage).width(1280).height(128).url()} width={1280} height={128} layout="fixed" />
+                </div>
+              </div>
+              <div className="fg-text z-10 rounded-lg flex flex-col md:flex-row justify-between items-center p-2 group relative" style={{ backgroundColor: 'rgba(0,0,0,0.25)' }}>
+                <div className="transform md:origin-left group-hover:scale-125 transition-all">
+                  <h1 className="text-2xl font-semibold">{post.title}</h1>
+                </div>
+                <div className="md:text-right transform md:origin-right group-hover:scale-105 transition-all">
+                  <div className="flex items-center justify-center md:justify-end gap-1">
+                    <Image src={urlFor(post.author.image).width(64).height(64).url()} width={16} height={16} className="rounded-full" />
+                    <span>{post.author.name}</span>
+                  </div>
+                  <p>{moment(post.publishedAt).format('HH:mm DD.MM.YYYY')}</p>
+                </div>
+                <Link href={`/blog/${post.slug.current}`}>
+                  <a className="flex-none absolute top-0 left-0 bottom-0 right-0"></a>
+                </Link>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
 export default Blog
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const data = await client.fetch(`
+    *[_type == "post"]{
+      _id,
+      author->{
+        name,
+        image
+      },
+      body,
+      mainImage,
+      publishedAt,
+      slug,
+      title
+    }
+  `);
+
+  const sortedData = [...data].sort(function (a:any, b:any) {
+    var dateA = new Date(a.publishedAt).getTime();
+    var dateB = new Date(b.publishedAt).getTime();
+    return dateA < dateB ? 1 : -1;
+  });
+
+  return {
+    props: {
+      posts: sortedData
+    },
+
+    revalidate: 60 * 30,
+  };
+}
