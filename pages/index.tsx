548c92b776e90567148e442bda31b4f531863a60
DerLev <levin.schroeren@gmail.com>
2021-09-26 12:29:27 +0200

    index.tsx done

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
       />
       <div className="overflow-hidden rounded-xl shadow-xl relative" style={{ height: '33rem' }}>
         <div className="absolute transform left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <Image src={bgImg} width={1248} layout="fixed" />
+          <Image src={bgImg} width={1248} layout="fixed" alt="Snowy Mountains" />
         </div>
         <div className="absolute z-10 top-0 left-0 bottom-0 right-0 p-4 flex items-center justify-center">
           <div className="text-center">
@@ -23,22 +23,22 @@ const Home: NextPage = () => {
             <h3 className="text-xl description-shadow mb-1">Social links:</h3>
             <div className="flex justify-center gap-1">
               <a href="https://twitter.com/_derlev_" target="_blank" rel="noopener noreferrer" className="group">
-                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800">
+                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800 shadow-md">
                   <FontAwesomeIcon icon={faTwitter} className="w-6 h-6 transition-all transform group-hover:scale-110" />
                 </div>
               </a>
               <a href="https://instagram.com/_derlev_" target="_blank" rel="noopener noreferrer" className="group">
-                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800">
+                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800 shadow-md">
                   <FontAwesomeIcon icon={faInstagram} className="w-6 h-6 transition-all transform group-hover:scale-110" />
                 </div>
               </a>
               <a href="https://discord.com/users/377103974081495042" target="_blank" rel="noopener noreferrer" className="group">
-                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800">
+                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800 shadow-md">
                   <FontAwesomeIcon icon={faDiscord} className="w-6 h-6 transition-all transform group-hover:scale-110" />
                 </div>
               </a>
               <a href="https://github.com/DerLev" target="_blank" rel="noopener noreferrer" className="group">
-                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800">
+                <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gray-800 shadow-md">
                   <FontAwesomeIcon icon={faGithub} className="w-6 h-6 transition-all transform group-hover:scale-110" />
                 </div>
               </a>
