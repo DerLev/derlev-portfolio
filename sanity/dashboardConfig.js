9f9bfb6d00089e6bdc700e59a67a066eeb0fac45
DerLev <levin.schroeren@gmail.com>
2021-10-06 22:54:12 +0200

    sanity studio dashboard done

--- sanity/dashboardConfig.js
+++ sanity/dashboardConfig.js
@@ -1,10 +1,29 @@
 export default {
   widgets: [
     {
+      name: 'project-info',
+      layout: {
+        width: 'medium'
+      }
+    },
+    {
+      name: 'document-list',
+      options: {
+        title: 'Latest Posts',
+        order: '_updatedAt desc',
+        types: ['post'],
+        limit: 5,
+        showCreateButton: false
+      },
+      layout: {
+        width: 'large'
+      }
+    },
+    {
       name: 'vercel',
       layout: {
         width: 'full',
       },
-    },
+    }
   ],
 }
